6d59688b19355adf8d516f3018b787667090e11d
GUILHERME PIRES VIANA <guilherme.viana@alunos.sc.senac.br>
2023-09-01 21:37:39 -0300

    restore

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 console.log('eu sou muito lindo');
 console.log('obrigado');
 console.log('novo teste');
-console.log('sejao foguetes');
-console.log('alterado');
--- index.js
+++
@@ -1,4 +0,0 @@
-function soma(n1,n2) {
-    console.log(n1+n2);
-}
-soma(1,2);
