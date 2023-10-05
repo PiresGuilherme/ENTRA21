3f48908f7b3a2df34f886a77c6b270cdd9802d50
PiresGuilherme <vianaguilhermepv@gmail.com>
2023-10-05 20:01:48 -0300

    atividade em js

--- aprendizagemJS/functionArray/functionArrayTeste.js
+++ aprendizagemJS/functionArray/functionArrayTeste.js
@@ -1,89 +1,89 @@
 
-    //  Filtrar números pares de um array
-    // const numeros = [1, 2, 3, 4, 5, 6];
-    // const numerosPares = numeros.filter(function (numero) {
-    //     return numero % 2 === 0;
-    // });
+//  Filtrar números pares de um array
+// const numeros = [1, 2, 3, 4, 5, 6];
+// const numerosPares = numeros.filter(function (numero) {
+//     return numero % 2 === 0;
+// });
 
-    // console.log(numerosPares); // Output: [2, 4, 6]
-
-
-    //  Elevar todos os números em um array ao quadrado
-    //  const numeros = [1, 2, 3, 4, 5];
-    //  const quadrados = numeros.map(function (numero) {
-    //      return numero ** 2;
-    //  });
-
-    //  console.log(quadrados);  Output: [1, 4, 9, 16, 25]
-
-    //  Calcular a soma de todos os números em um array
-    //  const numeros = [1, 2, 3, 4, 5];
-    //  const soma = numeros.reduce(function (acumulador, numero) {
-    //      return acumulador + numero;
-    //  }, 0);  0 é o valor inicial do acumulador
-
-    //  console.log(soma);  Output: 15
+// console.log(numerosPares); // Output: [2, 4, 6]
 
 
-    //  Imprimir todos os elementos de um array
-    //  const frutas = ["Maçã", "Banana", "Pêra", "Laranja"];
+//  Elevar todos os números em um array ao quadrado
+//  const numeros = [1, 2, 3, 4, 5];
+//  const quadrados = numeros.map(function (numero) {
+//      return numero ** 2;
+//  });
 
-    //  frutas.forEach((item,index) => console.log(item,index));
+//  console.log(quadrados);  Output: [1, 4, 9, 16, 25]
+
+//  Calcular a soma de todos os números em um array
+//  const numeros = [1, 2, 3, 4, 5];
+//  const soma = numeros.reduce(function (acumulador, numero) {
+//      return acumulador + numero;
+//  }, 0);  0 é o valor inicial do acumulador
+
+//  console.log(soma);  Output: 15
 
 
+//  Imprimir todos os elementos de um array
+//  const frutas = ["Maçã", "Banana", "Pêra", "Laranja"];
+
+//  frutas.forEach((item,index) => console.log(item,index));
 
 
 
 
 
-    ///////////////////////////////////////////////////////////
-    //  exercicio
-
-    //1
-    // const n1 = [ 1, 2, 3, 5, 4, 8, 6, 7, 5, 9]
-    // const t1 = n1.filter((n => n %2==0)) ;
-    // console.log(t1);
 
 
-    //2
-    // const n2 = ['guilherme', 'pires', 'viana'];
-    // const t2 = n2.map((n) => n.toLocaleUpperCase());
-    // console.log(t2);
+///////////////////////////////////////////////////////////
+//  exercicio
+
+//1
+// const n1 = [ 1, 2, 3, 5, 4, 8, 6, 7, 5, 9]
+// const t1 = n1.filter((n => n %2==0)) ;
+// console.log(t1);
 
 
-    //3
-    // const nomes = ['guilherme', 'pires', 'viana'];
-    // const saudacao = nomes.forEach(n => console.log(`prazer ${n}, tudo bem ? `));
+//2
+// const n2 = ['guilherme', 'pires', 'viana'];
+// const t2 = n2.map((n) => n.toLocaleUpperCase());
+// console.log(`Olá , ${t2}`);
 
 
-    // 4
-    // const substantivos = ['maca', 'banana', 'pessego' , 'saude', 'uva', 'mamao']
-    // const m = substantivos.filter(t => t.charAt(0)==='m').map((n) => n.toLocaleUpperCase());
-    // console.log(m);
+//3
+// const nomes = ['guilherme', 'pires', 'viana'];
+// const saudacao = nomes.forEach(n => console.log(`prazer ${n}, tudo bem ? `));
 
 
-    //5
-    // const notasAlunos = [
-    //     {nome: 'Pedro', notas: [8,9]},
-    //     {nome: 'Pires', notas: [9, 10]},
-    //     {nome: 'Pedro', notas: [2, 4]}
-    // ];
-    
-    // const media = notasAlunos.map((nota) => { 
-    //     const medias = nota.notas.reduce((i,a) => i + a, 0) /3;
+// 4
+// const substantivos = ['maca', 'banana', 'pessego' , 'saude', 'uva', 'mamao']
+// const m = substantivos.filter(t => t.toLowerCase(0).charAt(0)==='m').map((n) => n.toLocaleUpperCase());
+// console.log(m);
 
-    //     return {...nota,medias}
-    // });
-    const notasAlunos = [
-        {nome: 'Pedro', notas: [8,9]},
-        {nome: 'Pires', notas: [9, 10]},
-        {nome: 'Pedro', notas: [2, 4]}
-    ];
-    
-    const media = notasAlunos.map((nota) => { 
-        const medias = nota.notas.reduce((i,a) => i + a, 0) /3;
 
-        return {...nota,medias}
-    });
-    
-    console.log(media);
+//5
+// const notasAlunos = [
+//     { nome: 'Pedro', notas: [8, 9] },
+//     { nome: 'Pires', notas: [9, 10] },
+//     { nome: 'Pedro', notas: [2, 4] }
+// ];
+
+// const media = notasAlunos.map((nota) => {
+//     const medias = nota.notas.reduce((i, a) => i + a, 0) / 2;
+
+//     return { ...nota, medias }
+// });
+
+
+
+// let alunosAprovados = media.filter((aluno) => {aluno.medias >= 7;
+//    if (aluno.medias >= 7) {
+//     console.log(`A média de ${aluno.nome} é ${aluno.medias} e está aprovado`);
+//    }  else{
+//     console.log(`A média de ${aluno.nome} é ${aluno.medias} e está reprovado`);
+//    }
+// });
+   
+ 
+ 
---
+++ aprendizagemJS/objects/objects.js
@@ -0,0 +1,31 @@
+// var meuCarro = new Object();
+// meuCarro.fabricacao = 'Ford';
+// meuCarro.modelo = 'Mustang';
+// meuCarro.ano = 1969;
+
+
+
+// const carro = {
+//     marca: "Peugeot",
+//     modelo: "206",
+//     cor: "Prata",
+//     ano: 2014,
+//     acelerar: function () {
+//         console.log('acelerando...');
+//     }
+// }
+
+
+
+const pf = {
+    nome,
+    idade,
+    endereco,
+    estadoCivil
+};
+pf.nome = prompt('digite seu nome: ');
+pf.idade = prompt('digite sua idade: ');
+pf.endereco = prompt('digite seu endereco: ');
+pf.estadoCivil = prompt('digite seu estadoCivil: ');
+
+alert(`seu nome é ${pf.nome},sua idade é ${pf.idade},seu endereco é ${pf.endereco},seu estado civil é ${pf.estadoCivil}`);
