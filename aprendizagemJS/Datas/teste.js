44cd5080175fc8a2286e5de0debfc87b0cd48c83
PiresGuilherme <vianaguilhermepv@gmail.com>
2023-10-17 21:58:39 -0300

    finalização desafio, localStorage e Datas

---
+++ aprendizagemJS/Datas/teste.js
@@ -0,0 +1,6 @@
+const agora = new Date().toLocaleString('pt-BR');
+console.log(agora)
+
+const dataHoraBrasilia = agora.toLocaleString('pt-BR');
+
+console.log('Data e hora em Brasília:', dataHoraBrasilia);
---
+++ aprendizagemJS/localStorage/main.js
@@ -0,0 +1,34 @@
+// const mapa = new Map();
+
+// mapa.set('nome', 'Pires');
+// mapa.set('idade', 20);
+
+// console.log(mapa);
+// console.log(mapa.has('cidade'));
+
+
+// const set = new Map();
+
+// set.add(1);
+// set.add(2);
+// set.add(1);
+
+// console.log(set);
+
+// const carros = new Map();
+// carros.set(0, { 'fabricação': 1999, 'nome': 'Ford', 'cor': 'Vermelho' });
+// carros.set(1, { 'fabricação': 2005, 'nome': 'Gol', 'cor': 'Branco' });
+
+
+// console.log(carros.size);
+// var index = Number(prompt('digite o indice do carro que deseja alterar a cor'));
+// console.log(index);
+// console.log(carros);
+// let novaCor = prompt('Digite a nova cor');
+// console.log(carros.get(index).cor = novaCor);
+
+
+// const numeros = new Set([1,2,3,4,5,6]);
+// console.log(numeros);
+// numeros.add(7);
+// numeros.add(8);
