744403ff472fd969ecc6c718705676e39416ad03
PiresGuilherme <vianaguilhermepv@gmail.com>
2023-10-26 21:50:00 -0300

    aprendizagem async/await

--- aprendizagemJS/Callback/ex.js
+++ aprendizagemJS/Callback/ex.js
@@ -66,70 +66,191 @@
 
 
 // promises
-var array = [10, 9, 8, 7];
-const media = new Promise((resolve, err) => {
-    var valor = array.reduce((total, numero) => total + numero, 0);
+//1
+// var array = [10, 9, 8, 7];
+// const media = new Promise((resolve, err) => {
+//     var valor = array.reduce((total, numero) => total + numero, 0);
 
-    let total = (valor / array.length);
-    if (typeof total === 'number') {
-        resolve(total);
-    } else {
-        err('erro');
-    }
-})
+//     let total = (valor / array.length);
+//     if (typeof total === 'number') {
+//         resolve(total);
+//     } else {
+//         err('erro');
+//     }
+// })
 
-media.then((resultado) => {
-    console.log(resultado);
-}).catch((erro) => {
-    console.log(erro);
-})
+// media.then((resultado) => {
+//     console.log(resultado);
+// }).catch((erro) => {
+//     console.log(erro);
+// })
 
 
-//2 
-const principal2 = new Promise((resolve, reject) => {
-    let par = array.filter(par => (par % 2) == 0)
-    if (par.length > 0) {
-        resolve(par)
-    } else {
-        reject('não há nenhum número par')
-    }
-})
-principal2.then((pares) => {
-    console.log(pares);
-}).catch((err) => {
-    console.log(err);
-})
+// 2 
+// const principal2 = new Promise((resolve, reject) => {
+//     let par = array.filter(par => (par % 2) == 0)
+//     if (par.length > 0) {
+//         resolve(par)
+//     } else {
+//         reject('não há nenhum número par')
+//     }
+// })
+// principal2.then((pares) => {
+//     console.log(pares);
+// }).catch((err) => {
+//     console.log(err);
+// })
 
 
 //3
-const obterUser = new Promise((resolve, reject) => {
-    let user = {id: 1,
-        nome: 'Aladim',
-        dataNascimento: new Date()}
-        if (user != null ) {
-            return setTimeout(() => {
-                return resolve(user)
-            }, 1000)
-        } else {
-            reject('erro')
-        }
-});
-const obterTel = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve({
-            telefone: '470028922',
-            ddd: 11
-        })
-    }, 2000)
-})
+// const obterUser = new Promise((resolve, reject) => {
+//     let user = {id: 1,
+//         nome: 'Aladim',
+//         dataNascimento: new Date()}
+//         if (user != null ) {
+//             return setTimeout(() => {
+//                 resolve(user)
+//             }, 1000)
+//         } else {
+//             reject('erro')
+//         }
+// });
+// const obterTel = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve({
+//             telefone: '470028922',
+//             ddd: 11
+//         })
+//     }, 2000)
+// })
 
-obterUser.then((correto)=> {
-    console.log(correto.nome);
-    obterTel.then((ok)=>{
-        console.log(ok.telefone);
-    }).catch((err) => {
-        console.log(err);
-    })
-}).catch((erro)=>{
-    console.log(erro);
-})
+// obterUser.then((correto)=> {
+//     console.log(correto.nome);
+//     obterTel.then((ok)=>{
+//         console.log(ok.telefone);
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+// }).catch((erro)=>{
+//     console.log(erro);
+// })
+
+
+
+
+
+///ASYNC/AWAIT
+// var notas = [10, 9, 8, 7];
+// function media(array) {
+//     return new Promise((resolve, reject) => {
+//         var valor = array.reduce((total, numero) => total + numero, 0);
+//         valor /= array.length;
+//         if (typeof valor === 'number') {
+//             resolve(valor);
+//         }
+//     })
+// }
+// async function exec(array) {
+//     try {
+//         var retorno = await media(array);
+//         console.log(retorno);
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// }
+// exec(notas);
+
+
+// var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function pares(numeros) {
+//     return new Promise((resolve, reject) => {
+//         if (numeros.filter((n) => n % 2 == 0)) {
+//             let par = numeros.filter((n) => n % 2 == 0)
+//             resolve(par)
+//         } else {
+//             reject('Não há numeros pares');
+//         }
+//     })
+// }
+// async function calcular(array) {
+//     try {
+//         var filtrado = await pares(array);
+//         console.log(filtrado);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// calcular(nums);
+
+
+
+// var user = {
+//     id: 1,
+//     nome: 'Pires',
+//     Nascimento: new Date()
+// }
+// function cadastro(user) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (user !== null) {
+//                 resolve(user);
+//             } else {
+//                 reject(new Error('Usuário não existe'));
+//             }
+//         }, 1000)
+//     })
+// }
+// function telefone(id) {
+    // return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (typeof id === 'number') {
+//                 var telefone = {
+//                     numero: '98832-9003',
+//                     ddd: 47
+//                 };
+
+//                 resolve(telefone);
+//             } else {
+//                 reject(new Error('Não há telefone para este usuário'))
+//             }
+//         }, 2000)
+//     })
+// }
+
+// async function exibir(usuario) {
+//     try {
+//         var u = await cadastro(usuario);
+//         console.log(u.nome);
+//         var tel = await telefone(u.id);
+//         console.log(tel.ddd + " " + tel.numero);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// exibir(user);
+
+// function buscarDados() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(()=>{
+//             const dados ={
+//                 nome:"usuário exemplo",
+//                 idade: 25
+//             }
+//             if (Math.random() < 0.2) {
+//                 reject(new Error("erro ao obter dados da API"))
+//             } else {
+//                 resolve(dados);
+//             }
+//         }, 1000)
+//     })
+// }
+// async function processarDados() {
+//     try{
+//         var dado = await buscarDados();
+//         console.log(dado);
+//     } catch(erro) {
+//         console.log(erro);
+//     }
+// }
+// processarDados();
--- aprendizagemJS/Callback/explicacao.js
+++ aprendizagemJS/Callback/explicacao.js
@@ -167,3 +167,55 @@
 // }).catch((erro) => {
 //     console.log(erro.nome + ' ' + erro.message);
 // });
+
+
+
+//ASYNC/AWAIT
+// function melhorBandaDeRock(banda) {
+//     return new Promise((resolve, reject) => {
+//         if (banda == "Oficina G3") {
+//             resolve({
+//                 sucesso: true,
+//                 nomeDaBanda: banda,
+//                 msg: banda + "é a melhor banda de rock!"
+//             });
+//         } else {
+//             reject({
+//                 sucesso: false,
+//                 msg: "Não tenho certeza",
+//             });
+//         }
+//     });
+// }
+
+// function melhorMusica(resposta) {
+//     return new Promise((resolve, reject) => {
+//         if (resposta.sucesso) {
+//             resolve("Humanos de " + resposta.nomeDaBanda);
+//         } else {
+//             reject("Você conhece o oficina G3?");
+//         }
+//     });
+// }
+
+// melhorBandaDeRock("Oficina G3").then((response) => {
+//     console.log("Checkando a resposta...");
+//     return melhorMusica(response);
+// }).then(response => {
+//     console.log("Encontrando a melhor música");
+//     console.log(response);
+// }).catch(err => {
+//     console.log(err.msg);
+// })
+
+// async function facaOTrabalho() {
+//     try {
+//         const melhorBandaR = await melhorBandaDeRock('Oficina G3');
+//         console.log(melhorBandaR);
+//         const melhorMusicaR = await melhorMusica(melhorBandaR);
+//         console.log(melhorMusicaR);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// facaOTrabalho();
